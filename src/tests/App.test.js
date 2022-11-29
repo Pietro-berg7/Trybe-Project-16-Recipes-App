10ec430dc2c3b8b1e1f39d49a4765e56586588fa
Mark Allen <markallenarchviz@gmail.com>
2022-11-29 18:45:17 -0300

    Fix test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,18 +1,31 @@
 import React from 'react';
-// import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
 
-test('Rota da pagina de Login', () => {
-  const { history } = renderWithRouter(<App />);
+describe('testes', () => {
+  test('Rota da pagina de Login', () => {
+    const { history } = renderWithRouter(<App />);
 
-  const { pathname } = history.location;
-  expect(pathname).toBe('/');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('redireciona para rota "/meals", após login', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const enterBtn = screen.getByTestId('login-submit-btn');
+
+    const emailInput = screen.getByTestId('email-input');
+    userEvent.type(emailInput, 'mark@email.com');
+
+    const passwordInput = screen.getByTestId('password-input');
+    userEvent.type(passwordInput, '1234567');
+
+    userEvent.click(enterBtn);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/meals');
+  });
 });
-
-// test('Validação de email e password', () => {
-//   const { history } = renderWithRouter(<App />);
-
-//   const { pathname } = history.location;
-//   expect(pathname).toBe('/');
-// });
