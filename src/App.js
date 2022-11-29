c08ff5ddf4e39a6b836cf57d2367436d81b9c0b2
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-11-29 20:59:55 -0200

    Requisitos 17 e 18 concluídos - Componentes criados e rotas prontas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import Recipe from './pages/Recipe';
+import RecipeInProgress from './pages/RecipeInProgress';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Footer />
+    <Switch>
+      {/* <Route exact path="/" component={ Login } /> */}
+      <Route exact path="/meals/:id" component={ Recipe } />
+      <Route exact path="/drinks/:id" component={ Recipe } />
+      <Route path="/meals/:id/in-progress" component={ RecipeInProgress } />
+      <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+      <Route exact path="/meals" component={ Meals } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/profile" component={ Profile } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ Favorites } />
+      <Route path="*" component={ NotFound } />
+    </Switch>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import '../css/Footer.css';
 
 export default function Footer() {
+  const history = useHistory();
+
+  const DrinksPage = () => {
+    history.push('/drinks');
+  };
+
+  const MealsPage = () => {
+    history.push('/meals');
+  };
+
   return (
     <footer data-testid="footer" className="footer">
       <button
         data-testid="drinks-bottom-btn"
         type="button"
         src={ drinkIcon }
+        onClick={ DrinksPage }
       >
         <img src={ drinkIcon } alt="Drink Icon" />
       </button>
@@ -17,6 +29,7 @@ export default function Footer() {
         data-testid="meals-bottom-btn"
         type="button"
         src={ mealIcon }
+        onClick={ MealsPage }
       >
         <img src={ mealIcon } alt="Meal Icon" />
       </button>
