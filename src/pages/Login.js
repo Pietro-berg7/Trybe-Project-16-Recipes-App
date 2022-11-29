c059a0ff0d02d8772d878553de442cde281cecd8
Mark Allen <markallenarchviz@gmail.com>
2022-11-29 17:36:54 -0300

    Login form validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  const handleFormValidation = () => {
+    const { email, password } = form;
+    return !(/.+@.+\.[A-Za-z]+$/.test(email) && /^(?=.*\d).{6,}$/.test(password));
+  };
   return (
-    <main className="login-page">
-      <p>Login!</p>
+    <main className="login-main">
+      <section>
+        <form>
+          <input
+            placeholder="Email"
+            type="email"
+            data-testid="email-input"
+            name="email"
+            value={ form.email }
+            onChange={ ({ target }) => setForm({ ...form, [target.name]: target.value }) }
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            data-testid="password-input"
+            name="password"
+            value={ form.password }
+            onChange={ ({ target }) => setForm({ ...form, [target.name]: target.value }) }
+          />
+          <button
+            data-testid="login-submit-btn"
+            type="button"
+            disabled={ handleFormValidation() }
+          >
+            Enter
+
+          </button>
+        </form>
+      </section>
     </main>
   );
 }
