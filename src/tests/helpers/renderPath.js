f290eac56d4eca04fa3cdd748af59aabf0a1cb01
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-12-01 06:24:17 -0300

    fix: helper functions to include provider

--- src/tests/helpers/renderPath.js
+++ src/tests/helpers/renderPath.js
@@ -3,14 +3,17 @@ import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { render } from '@testing-library/react';
 import App from '../../App';
+import Provider from '../../context/useContext';
 
 const renderPath = (path) => {
   const history = createBrowserHistory();
   history.push(path);
   const { ...resources } = render(
-    <Router history={ history }>
-      <App />
-    </Router>,
+    <Provider>
+      <Router history={ history }>
+        <App />
+      </Router>
+    </Provider>,
   );
   return { ...resources, history };
 };
