b81688ee1448bf9e65f6bfba2a3574543129be40
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-12-03 06:02:54 -0300

    Requisito 25 em progresso

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -18,6 +18,38 @@ export default function Recipe() {
   console.log(recipe);
 
   return (
-    <main />
+    recipe && (
+      <main>
+        <img data-testid="recipe-photo" src={ recipe.strMealThumb } alt="" width="100%" />
+        <h1 data-testid="recipe-title">{ recipe.strMeal }</h1>
+        <h3 data-testid="recipe-category">{ recipe.strCategory }</h3>
+        <h2>Ingredients</h2>
+        <ul>
+          {
+            Object.entries(recipe)
+              .filter((ingredient) => (ingredient[0].includes('Ingredient')))
+              .filter((ingredient) => (ingredient[1] !== ''))
+              .map((ingredient) => (
+                <li
+                  data-testid={ `${ingredient[0]}-ingredient-name-and-measure` }
+                  key={ ingredient[0] }
+                >
+                  {ingredient[1]}
+                </li>
+              ))
+          }
+        </ul>
+        <p data-testid="instructions">{ recipe.strInstructions }</p>
+        <iframe
+          title="video"
+          data-testid="video"
+          width="100%"
+          src={ `https://www.youtube.com/embed/${recipe.strYoutube.split('=')[1]}` }
+          frameBorder="0"
+          allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </main>
+    )
   );
 }
