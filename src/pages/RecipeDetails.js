4f36882fa19c50a08f92b5050f147de32ef5c878
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-12-05 20:07:16 -0300

    Requisito 25 em progresso - Precisa renderizar ingredientes e quantidades

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -20,35 +20,56 @@ export default function Recipe() {
   return (
     recipe && (
       <main>
-        <img data-testid="recipe-photo" src={ recipe.strMealThumb } alt="" width="100%" />
-        <h1 data-testid="recipe-title">{ recipe.strMeal }</h1>
-        <h3 data-testid="recipe-category">{ recipe.strCategory }</h3>
-        <h2>Ingredients</h2>
-        <ul>
-          {
-            Object.entries(recipe)
-              .filter((ingredient) => (ingredient[0].includes('Ingredient')))
-              .filter((ingredient) => (ingredient[1] !== ''))
-              .map((ingredient) => (
-                <li
-                  data-testid={ `${ingredient[0]}-ingredient-name-and-measure` }
-                  key={ ingredient[0] }
-                >
-                  {ingredient[1]}
-                </li>
-              ))
-          }
-        </ul>
-        <p data-testid="instructions">{ recipe.strInstructions }</p>
-        <iframe
-          title="video"
-          data-testid="video"
+        <img
+          data-testid="recipe-photo"
+          src={ recipe.strMealThumb || recipe.strDrinkThumb }
+          alt=""
           width="100%"
-          src={ `https://www.youtube.com/embed/${recipe.strYoutube.split('=')[1]}` }
-          frameBorder="0"
-          allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
         />
+        <h1 data-testid="recipe-title">{ recipe.strMeal || recipe.strGlass }</h1>
+        <h3 data-testid="recipe-category">{ recipe.strCategory }</h3>
+        { recipe.strAlcoholic && (<p>{ recipe.strAlcoholic }</p>)}
+        <h2>Ingredients</h2>
+        {
+          Object.entries(recipe)
+            .filter((ingredient) => (ingredient[0].includes('Ingredient')))
+            .filter((ingredient) => (ingredient[1] !== ''))
+            .map((ingredient) => (
+              <li
+                data-testid={ `${ingredient[0]}-ingredient-name-and-measure` }
+                key={ ingredient[0] }
+              >
+                {ingredient[1]}
+              </li>
+            ))
+        }
+
+        <p data-testid="instructions">{ recipe.strInstructions }</p>
+        {
+          recipe.strYoutube
+            ? recipe.strYoutube !== null && (
+              <iframe
+                title="video"
+                data-testid="video"
+                width="100%"
+                src={ `https://www.youtube.com/embed/${recipe.strYoutube.split('=')[1]}` }
+                frameBorder="0"
+                allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            )
+            : recipe.strVideo !== null && (
+              <iframe
+                title="video"
+                data-testid="video"
+                width="100%"
+                src={ `https://www.youtube.com/embed/${recipe.strVideo.split('=')[1]}` }
+                frameBorder="0"
+                allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            )
+        }
       </main>
     )
   );
