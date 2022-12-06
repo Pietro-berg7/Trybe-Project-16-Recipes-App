2d75fade145308fa29ee870ce9543a111c2fbadc
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-12-06 09:48:53 -0300

    feat: (req 57)  profile page basic structure

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,32 @@ export default function Profile() {
   return (
     <div>
       <Header />
+      <main>
+        <p data-testid="profile-email">email</p>
+        <section>
+          <button
+            data-testid="profile-done-btn"
+            type="button"
+            onClick={ () => {} }
+          >
+            Done Recipes
+          </button>
+          <button
+            data-testid="profile-favorite-btn"
+            type="button"
+            onClick={ () => {} }
+          >
+            Favorite Recipes
+          </button>
+          <button
+            data-testid="profile-logout-btn"
+            type="button"
+            onClick={ () => {} }
+          >
+            Logout
+          </button>
+        </section>
+      </main>
       <Footer />
     </div>
   );
