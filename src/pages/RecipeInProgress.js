e5b96d42b644c28c9ff96c54b93cde4184887f6e
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-12-06 15:55:36 -0300

    feat: recipe in progress page elements

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-export default function RecipeInProgress() {
+export default function RecipeInProgress(props) {
+  const { match: { params: { id } } } = props;
+
   return (
-    <div>RecipeInProgress</div>
+    <div>
+      <h1>RecipeInProgress</h1>
+      <h2 data-testid="recipe-title">{id}</h2>
+      <img
+        data-testid="recipe-photo"
+        src=""
+        alt="Recipe"
+      />
+      <div>
+        <button
+          data-testid="share-btn"
+          type="button"
+          onClick={ () => {} }
+        >
+          Compartilhar
+        </button>
+        <button
+          data-testid="favorite-btn"
+          type="button"
+          onClick={ () => {} }
+        >
+          Favoritar
+        </button>
+      </div>
+      <p data-testid="recipe-category">Categoria</p>
+      <span data-testid="instructions">
+        Instruções
+      </span>
+      <button
+        data-testid="finish-recipe-btn"
+        type="button"
+        onClick={ () => {} }
+      >
+        Finalizar Receita
+      </button>
+    </div>
   );
 }
+
+RecipeInProgress.propTypes = {
+  match: propTypes.shape().isRequired,
+};
