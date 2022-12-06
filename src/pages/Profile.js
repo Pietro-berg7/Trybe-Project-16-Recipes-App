a17fa5434560739d83af0f012b4112165f48a5cf
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-12-06 09:59:43 -0300

    feat: (Req 58) recover email from local storage

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const [userMail, setUserMail] = useState('');
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const { email } = user;
+    setUserMail(email);
+  }, []);
+
   return (
     <div>
       <Header />
       <main>
-        <p data-testid="profile-email">email</p>
+        <p data-testid="profile-email">{ userMail }</p>
         <section>
           <button
             data-testid="profile-done-btn"
