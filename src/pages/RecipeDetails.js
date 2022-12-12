5cdb8954fe7aa2e47b68d5e827dfa1ad3a0799d8
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-12-12 15:03:16 -0300

    Conserta requisito 36

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,4 +1,3 @@
-/* eslint-disable complexity */
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { object } from 'prop-types';
@@ -13,8 +12,8 @@ export default function RecipeDetails(props) {
   const { pathname } = history.location;
   const { fetchRecipeId } = useContext(Context);
   const [recipe, setRecipe] = useState();
-  const [startRecipeBtn] = useState(false);
-  const done = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [startRecipeBtn, setStartRecipeBtn] = useState(false);
+  const [done, setDone] = useState(false);
 
   useEffect(() => {
     const fetchRecipeById = async () => {
@@ -25,87 +24,97 @@ export default function RecipeDetails(props) {
   }, [setRecipe, fetchRecipeId, pathname, recipe]);
 
   useEffect(() => {
-    const prevFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    if (prevFavorites === null) {
-      window.localStorage.setItem('favoriteRecipes', JSON.stringify([]));
+    const inProgress = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    if (inProgress !== null) {
+      const type = pathname.includes('drinks') ? 'drinks' : 'meals';
+
+      if (inProgress[type] !== null && inProgress[type] !== undefined) {
+        const exists = Object.keys(inProgress[type]).includes(id);
+        setStartRecipeBtn(exists);
+      }
     }
-    window.localStorage.setItem('doneRecipes', JSON.stringify([]));
-  }, []);
+    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    if (doneRecipes === null) {
+      localStorage.setItem('doneRecipes', JSON.stringify([]));
+    }
+    const recipeDone = JSON.parse(localStorage.getItem('doneRecipes'));
+    const isDone = recipeDone.find((element) => element.id === id) !== undefined;
+    setDone(isDone);
+  }, [pathname, id]);
 
   const handleStartRecipe = () => {
     history.push(`${history.location.pathname}/in-progress`);
   };
 
+  if (!recipe) return null;
+
   return (
-    recipe && (
-      <main>
-        <img
-          data-testid="recipe-photo"
-          src={ recipe.strMealThumb || recipe.strDrinkThumb }
-          alt=""
+    <main>
+      <img
+        data-testid="recipe-photo"
+        src={ recipe.strMealThumb || recipe.strDrinkThumb }
+        alt=""
+        width="100%"
+      />
+      <h1 data-testid="recipe-title">{ recipe.strMeal || recipe.strDrink }</h1>
+      <h3 data-testid="recipe-category">
+        { recipe.strAlcoholic || recipe.strCategory }
+      </h3>
+      <h2>Ingredients</h2>
+      { Object.entries(recipe)
+        .filter((element) => (element[0].includes('Ingredient')))
+        .map((ingredient, index) => (
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+            key={ index }
+          >
+            {ingredient[1]}
+          </p>
+        ))}
+      <h2>Measures</h2>
+      { Object.entries(recipe)
+        .filter((element) => (element[0].includes('Measure')))
+        .map((measure, index) => (
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+            key={ index }
+          >
+            {measure[1]}
+          </p>
+        ))}
+      <h2>Instructions</h2>
+      <p data-testid="instructions">{ recipe.strInstructions }</p>
+      {recipe.strYoutube && (
+        <iframe
+          title="video"
+          data-testid="video"
           width="100%"
+          src={ `https://www.youtube.com/embed/${recipe.strYoutube.split('=')[1]}` }
+          frameBorder="0"
+          allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
         />
-        <h1 data-testid="recipe-title">{ recipe.strMeal || recipe.strDrink }</h1>
-        <h3 data-testid="recipe-category">
-          { recipe.strAlcoholic || recipe.strCategory }
-        </h3>
-        <h2>Ingredients</h2>
-        { Object.entries(recipe)
-          .filter((element) => (element[0].includes('Ingredient')))
-          .map((ingredient, index) => (
-            <p
-              data-testid={ `${index}-ingredient-name-and-measure` }
-              key={ index }
-            >
-              {ingredient[1]}
-            </p>
-          ))}
-        <h2>Measures</h2>
-        { Object.entries(recipe)
-          .filter((element) => (element[0].includes('Measure')))
-          .map((measure, index) => (
-            <p
-              data-testid={ `${index}-ingredient-name-and-measure` }
-              key={ index }
-            >
-              {measure[1]}
-            </p>
-          ))}
-        <h2>Instructions</h2>
-        <p data-testid="instructions">{ recipe.strInstructions }</p>
-        {recipe.strYoutube && (
-          <iframe
-            title="video"
-            data-testid="video"
-            width="100%"
-            src={ `https://www.youtube.com/embed/${recipe.strYoutube.split('=')[1]}` }
-            frameBorder="0"
-            allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        )}
-        <div>
-          {
-            done
-              .includes(recipe.idMeal || recipe.idDrink)
-              ? null
-              : (
-                <button
-                  data-testid="start-recipe-btn"
-                  type="button"
-                  style={ { position: 'fixed', bottom: '0px' } }
-                  onClick={ handleStartRecipe }
-                >
-                  { startRecipeBtn ? ('Start Recipe') : ('Continue Recipe') }
-                </button>
-              )
-          }
-        </div>
-        <ButtonShare pathname={ pathname } testid="btn-svg" />
-        <ButtonFavorite pathname={ pathname } recipe={ recipe } id={ id } />
-        <Recommendations />
-      </main>
-    )
+      )}
+      <div>
+        {
+          done
+            ? null
+            : (
+              <button
+                data-testid="start-recipe-btn"
+                type="button"
+                style={ { position: 'fixed', bottom: '0px' } }
+                onClick={ handleStartRecipe }
+              >
+                { startRecipeBtn ? ('Continue Recipe') : ('Start Recipe') }
+              </button>
+            )
+        }
+      </div>
+      <ButtonShare pathname={ pathname } />
+      <ButtonFavorite pathname={ pathname } recipe={ recipe } id={ id } />
+      <Recommendations />
+    </main>
   );
 }
 
